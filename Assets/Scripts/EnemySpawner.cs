46e156014092596a09194315cde3a1805c1b9b95
Fikran Akbar <fikranakbar756@gmail.com>
2020-05-02 20:01:01 +0700

    WaveConfig from enemyPathing.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -19,11 +19,12 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
         {
-            Instantiate(
-            waveConfig.GetEnemyPrefab(),
-            waveConfig.GetWaypoints()[0].transform.position,
-            Quaternion.identity
-            );
+            var newEnemy = Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity
+                );
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
         }
 
